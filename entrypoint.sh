13f35a06b5f66d7d3417114b1202309df8560d59
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-07-19 12:34:00 +0200

    WIP3

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ cp /app/config/autoload/bot.local.php.dist /app/config/autoload/bot.local.php
 cp /app/config/autoload/db.local.php.dist /app/config/autoload/db.local.php
 cp /app/config/autoload/session.local.php.dist /app/config/autoload/session.local.php
 cp /app/vendor/qbonaventure/discord-website-trello/config/trello.local.php.dist /app/config/autoload/trello.local.php
-cp /app/vendor/qbonaventure/discord-website-pushover/pushover.local.php.dist /app/config/autoload/pushover.local.php
+cp /app/vendor/qbonaventure/discord-website-pushover/config/pushover.local.php.dist /app/config/autoload/pushover.local.php
 
 exec php-fpm
